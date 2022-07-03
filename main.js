2e428e64fb8cb6b177027b8c1e204fb18a33547c
Andres <contactandresrangel@gmail.com>
2022-07-03 16:53:35 -0500

    Fix the basic round function. Have had some issues to develop the loop feature

--- main.js
+++ main.js
@@ -16,70 +16,78 @@ function computerPlay() {
     } else {
         computerChoice = 'scissors';
     }
-    console.log('The CPU choose: ' + computerChoice)
 
     return computerChoice;
 }
+
+console.log('The CPU choose: ' + computerSelection)
+
 
 // Obtains the player's choice
 function playerPlay() {
     playerChoice = prompt('Type an option:  ["Rock", "Paper" or "Scissors"]').toLowerCase();
     
-    console.log('The player choose: ' + playerChoice);
+    // console.log('The player choose: ' + playerChoice);
 
 
     return playerChoice;
 }
+
+console.log('The player choose: ' + playerSelection)
+
 
 // Evaluates the winner of the round
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === playerSelection) {
         computerScore ++;
         playerScore ++;
-        result = 'It\'s a tie! ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'It\'s a tie! ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         
     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
         computerScore ++;
-        result = 'Computer win! Rock beats Scissors '  + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'Computer win! Rock beats Scissors '  + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         
     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
         computerScore ++;
-        result = 'Computer win! Paper beats Rock ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'Computer win! Paper beats Rock ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         
     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
         computerScore ++;
-        result = 'Computer win! Scissors beats Paper ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'Computer win! Scissors beats Paper ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore ++;
-        result = 'You win! Rock beats Scissors ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'You win! Rock beats Scissors ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore ++;
-        result = 'You win! Paper beats Rock ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'You win! Paper beats Rock ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore ++;
-        result = 'You win! Scissors beats Paper ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
+        return 'You win! Scissors beats Paper ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
         }
-    return result;
 }
 
-// Game loop - continues until the player or the computer reaches 5 points
-function game() {
-    for (let i = 0; i < 5; i++) {
-        computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 
 
-        playerPlay();
+
+// // Game loop - continues until the player or the computer reaches 5 points
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         computerPlay();
+
+
+//         playerPlay();
         
         
-        console.log(playRound(playerSelection, computerSelection));
+//         console.log(playRound(playerSelection, computerSelection));
 
 
-    }
-}
+//     }
+// }
 
 
 
-game();   
+// game();   
