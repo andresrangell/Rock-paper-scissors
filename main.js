4de2874e017b712f4719d4b5e519aa9c2bf472c9
Andres <contactandresrangel@gmail.com>
2022-07-04 14:06:41 -0500

    Add documentation

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const choices = ['rock', 'paper', 'scissors'];
 const winners = [];
 
 
+/**
+The function game() plays a round of rock, paper, scissors, and logs the results
+ */
 function game() {
 	for (let i = 0; i <= 5; i++) {
 		playRound(i);
@@ -9,15 +12,23 @@ function game() {
 	logWins();
 }
 
+/**
+ * The function plays a round of rock, paper, scissors, and logs the results.
+ * @param round - the current round number
+ */
 function playRound(round) {
 	const playerSelection = playerChoice();
 	const computerSelection = computerChoice();
 	const winner = checkWinner(playerSelection, computerSelection);
 	winners.push(winner);
 	logRound(playerSelection, computerSelection, winner, round)
-
 }
 
+/**
+ * This function prompts the user to input their choice of rock, paper, or scissors. It then validates
+ * the input and returns the input if it is valid.
+ * @returns The player's choice of rock, paper, or scissors
+ */
 function playerChoice() {
 	let input = prompt('Type Rock, Paper, or Scissors');
 	while (input == null) {
@@ -39,14 +50,32 @@ function playerChoice() {
 	return input;
 }
 
+/**
+ * The function computerChoice() returns a random choice from the choices array.
+ * @returns The computerChoice function is returning a random choice from the choices array.
+ */
 function computerChoice() {
 	return choices[Math.floor(Math.random() * choices.length)]
 }
 
+/**
+ * It takes a string as an argument and returns true if the string is in the array choices.
+ * @param choice - The user's choice.
+ * @returns a boolean value.
+ */
 function validateInput(choice) {
 	return (choices.includes(choice));
 }
 
+/**
+ * If the player and computer choices are the same, it's a tie. Otherwise, if the player has chosen
+ * rock and the computer has chosen scissors, or the player has chosen paper and the computer has
+ * chosen rock, or the player has chosen scissors and the computer has chosen paper, then the player
+ * wins. Otherwise, the computer wins
+ * @param choiceP - The player's choice
+ * @param choiceC - The computer's choice
+ * @returns a string.
+ */
 function checkWinner(choiceP, choiceC) {
 	if (choiceP === choiceC) {
 		return 'Tie';
@@ -61,6 +90,10 @@ function checkWinner(choiceP, choiceC) {
 	}
 }
 
+/**
+ * The function `logWins()` takes the array `winners` and filters it for the number of times the player
+ * won, the computer won, and the number of ties.
+ */
 function logWins() {
 	let playerWins = winners.filter((item) => item == 'Player').length;
 	let computerWins = winners.filter((item) => item == 'Computer').length;
@@ -71,6 +104,14 @@ function logWins() {
 	console.log('Ties:', ties)
 }
 
+/**
+ * The function takes in four parameters, logs the round number, the player's choice, the computer's
+ * choice, and the winner of the round
+ * @param playerChoice - The player's choice
+ * @param computerChoice - The computer's choice for the round
+ * @param winner - The winner of the round
+ * @param round - The current round number
+ */
 function logRound(playerChoice, computerChoice, winner, round) {
 	console.log('Round', round)
 	console.log('Player Choose:', playerChoice);
@@ -80,106 +121,3 @@ function logRound(playerChoice, computerChoice, winner, round) {
 }
 
 game();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let playerScore = 0;
-// let computerScore = 0;
-
-// let playerSelection = playerPlay();
-// let computerSelection = computerPlay();
-
-// // Generates the computer's choice ramdomly between rock, paper and scissors
-// function computerPlay() {
-//     let computerChoice = Math.floor(Math.random() * 3);
-
-//     if (computerChoice == 0) {
-//         computerChoice = 'rock';
-//     }  else if (computerChoice == 1) {
-//         computerChoice = 'paper';
-//     } else {
-//         computerChoice = 'scissors';
-//     }
-
-//     return computerChoice;
-// }
-
-
-
-
-// // Obtains the player's choice
-// function playerPlay() {
-//     playerChoice = prompt('Type an option:  ["Rock", "Paper" or "Scissors"]').toLowerCase();
-
-
-//     return playerChoice;
-// }
-
-
-
-// // Evaluates the winner of the round
-// function playRound(playerSelection, computerSelection) {
-//     if (computerSelection === playerSelection) {
-//         computerScore ++;
-//         playerScore ++;
-//         return 'It\'s a tie! ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-
-//     } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-//         computerScore ++;
-//         return 'Computer win! Rock beats Scissors '  + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-
-//     } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-//         computerScore ++;
-//         return 'Computer win! Paper beats Rock ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-
-//     } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-//         computerScore ++;
-//         return 'Computer win! Scissors beats Paper ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-
-//     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-//         playerScore ++;
-//         return 'You win! Rock beats Scissors ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-
-//     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-//         playerScore ++;
-//         return 'You win! Paper beats Rock ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-
-//     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-//         playerScore ++;
-//         return 'You win! Scissors beats Paper ' + 'Player score is: ' + playerScore + ' Computer score is: ' + computerScore;
-//         }
-// }
-
-// // console.log(playRound(playerSelection, computerSelection));
-
-
-
-// // Game loop - continues until the player or the computer reaches 5 points
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-
-//         playerPlay();
-//         console.log('The player choose: ' + playerSelection);
-
-//         computerPlay();
-//         console.log('The CPU choose: ' + computerSelection);
-
-//         console.log(playRound(playerSelection, computerSelection));
-
-
-//     }
-// }
-
-
-
-// game();
